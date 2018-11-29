13cbbaf167dd5bd34af88401333c77febc06e9b9
Jan Belterman <jjabelte@avans.nl>
2018-11-29 20:12:09 +0100

    Neo4j database test

--- test/friendships_test.js
+++ test/friendships_test.js
@@ -9,8 +9,8 @@ describe('/api/friendships', () => {
     beforeEach(async (done) => {
         let session = instance.session()
         await session.run('CREATE (:Person{username: "testUser1"}), (:Person{username:"testUser2"})')
-        const userOne = new User({username: "testUser1", password: "12345"})
-        const userTwo = new User({username: "testUser2", password: "12345"})
+        const userOne = new User({ username: "testUser1", password: "12345" })
+        const userTwo = new User({ username: "testUser2", password: "12345" })
         await userOne.save()
         await userTwo.save()
         done()
@@ -25,9 +25,27 @@ describe('/api/friendships', () => {
                     usernameOne: "testUser1",
                     usernameTwo: "testUser2"
                 })
-                .end(function (err, response) {
+                .end((err, response) => {
                     assert(response.status === 200)
                     done()
+                })
+        })
+
+        it('should create a new friendship with a valid request', (done) => {
+            request(app)
+                .post('/api/friendships')
+                .send({
+                    usernameOne: "testUser1",
+                    usernameTwo: "testUser2"
+                })
+                .end(() => {
+                    let session = instance.session()
+                    session.run('MATCH (:Person{username: "testUser1"})-[:friendsWith]-(p:Person) RETURN p')
+                        .then((result) => {
+                            const name = result.records[0]._fields[0].properties.username
+                            assert(name === 'testUser2')
+                            done()
+                        })
                 })
         })
 
