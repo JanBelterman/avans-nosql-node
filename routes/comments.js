b6c9c54feb3b180ba939522ea856300a7e31aded
Chris <christian-boer@hotmail.com>
2018-11-29 17:47:47 +0100

    fix merge

--- routes/comments.js
+++ routes/comments.js
@@ -4,16 +4,9 @@ const validateId = require('../middleware/validateId')
 
 const router = express.Router()
 
-<<<<<<< HEAD
 router.post('/:commentId?', commentsController.create)
 // router.delete('/:cid', validateId, commentsController.delete)
 // router.post('/:cid', commentsController.upvote)
 // router.post('/:cid', commentsController.downvote)
-=======
-router.post('/:cid?', commentsController.create)
-// router.delete('/:id', validateId, commentsController.delete)
-// router.post('/:id', commentsController.upvote)
-// router.post('/:id', commentsController.downvote)
->>>>>>> c4e95f21f8d037818f08148704c217a39db545b9
 
 module.exports = router
